8615aa357509e5a5e27e8f0247aabbfa557d9d54
Ruslan Bely <baiso@ya.ru>
2023-10-18 06:40:35 +0000

    add react-query

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,6 +1,7 @@
+import { AppProvider } from './app-providers'
 import './globals.css'
 import type { AppProps } from 'next/app'
 
-export  function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export function App({ Component, pageProps }: AppProps) {
+    return <AppProvider><Component {...pageProps} /></AppProvider>
 }
